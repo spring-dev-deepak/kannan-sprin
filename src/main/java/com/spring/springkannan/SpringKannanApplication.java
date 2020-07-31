de43e75dbb9558b9c54e33fc801ca3934d23e11c
spring-dev-deepak <65223428+spring-dev-deepak@users.noreply.github.com>
2020-07-31 15:31:28 +0530

    Initial Commit.

---
+++ src/main/java/com/spring/springkannan/SpringKannanApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springkannan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKannanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKannanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/springkannan/SpringKannanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springkannan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKannanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
